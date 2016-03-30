c95b85c98b2831ede408a1eb8448e3a76d738eb9
fmm <francisco.motrel@gft.com>
2016-03-30 17:39:05 +0200

    hola

---
+++ new-br.js
@@ -0,0 +1,16 @@
+
+function Fumar() {
+	console.log('foo');
+}
+
+
+
+
+baz();
+
+function examenDeFrancisco(){
+
+function Fumar() {
+	console.log('este log sustituye la linea del siguiente log');
+
+}
