7cec359a2e1130a6e0eef1d1ae25d92d8c34977a
Alejandro <alejandro.gomez@adesis.com>
2016-03-30 16:28:43 +0200

    (fix): volvemos a meter las funciones eliminadas

--- foo.js
+++ foo.js
@@ -7,6 +7,36 @@ function Fumar() {
 
 
 
+function baz() {
+	console.log('baz');
+}
+
+function alberto() {
+	
+	console.log('alberto funcion');
+}
+
+function stash2(){
+
+	//sfdds
+}
+
+function stash3Alex(){
+
+}
+
+alex();
+function alex(){
+
+	console.log('alejandro');
+}
+
+foo();
 
 
+function comer() {
+	return new Date();
+}
 
+
+baz();
