23924b607dcbf1c977ea9e0d141b91c0a2558430
Alejandro <alejandro.gomez@adesis.com>
2016-03-30 14:31:23 +0200

    (feat): se escribe la llamada a la funcion alex

--- foo.js
+++ foo.js
@@ -4,6 +4,34 @@ function Fumar() {
 
 
 
+
+
+
+function baz() {
+	console.log('baz')
+}
+
+function alberto() {
+	
+	console.log('alberto funcion')
+}
+
+function stash2(){
+
+	//sfdds
+}
+alex();
+
+
+
+function alex(){
+
+	console.log('alejandro');
+}
+
+foo();
+
+
 function comer() {
 	return new Date();
 }
