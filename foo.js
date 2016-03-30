17f6d4648e91855d460331f5fd91e1ccb6532dd5
Alejandro <alejandro.gomez@adesis.com>
2016-03-30 16:42:35 +0200

    (fix): funcion fumar en pipa en la rama de fran

--- foo.js
+++ foo.js
@@ -1,12 +1,8 @@
 function Fumar() {
+	console.log('este log sustituye la linea del siguiente log');
 	console.log('foo');
 }
 
-
-
-
-
-
-
-
-
+function fumarEnPipa(){
+	console.log('¿Puedo yo también fumar en pipa?');
+}
