05dd0617bde6adba9a2ed8306a0ef622d7e71df9
Alejandro <alejandro.gomez@adesis.com>
2016-03-30 16:43:46 +0200

    (fix): forzamos error al mover la funcion de arriba a abajo

--- foo.js
+++ foo.js
@@ -1,8 +1,11 @@
-function Fumar() {
-	console.log('este log sustituye la linea del siguiente log');
-	console.log('foo');
-}
+
 
 function fumarEnPipa(){
 	console.log('¿Puedo yo también fumar en pipa?');
+}
+
+
+function Fumar() {
+	console.log('este log sustituye la linea del siguiente log');
+	console.log('foo');
 }
